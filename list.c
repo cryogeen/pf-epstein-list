b22f52655de4b5b434b8ef970a9e5d7d09423754
vrillionaire <gerdientjhu@gmail.com>
2026-01-09 10:21:57 +0100

    changed extension for highlighting, added reasons

---
+++ list.c
@@ -0,0 +1,47 @@
+// yw
+// formatting is pretty simple
+// just remove all text after a // like C
+// then just loop over the list and ban them with your bot
+
+// to request addition / removal, open an issue on the repo
+
+589083242322460673 // derlefa // odd individual
+556763391415418887 // lapps // defends cp
+//132777808362471424 // navy
+151934018760343552 // lito // chinese
+153192575455592448 // raspy // nerd
+//272141622693593098 // iivanguard
+//187613017733726210 // d3v
+80798961836752896 // axis // furry
+//151484801667432449 // wingz
+84517868548411392 // shay // gooner
+496212609972830208 // celest // mod that cant ban someone for posting illegal material in stylis after it was deleted 🤡🤡🤡🤡
+//505254829208371201 // lunaris
+//403913463648354304 // npato
+//213057364738310144 // shiro
+563007363557097473 // smrkndev
+93518731568746496 // shay alt
+225662572131647489 // grace / orange. // sent cocker to minor
+363482567451738116 // soundestbread90 // defends child porn
+748173925942558731 // modraws // sent fetish material in a server with minors
+1201828584302596116 // mipacem7 // defends child porn
+310496470325526548 // noelle81 // too open with his porn addiction to minors (didnt even ask my age)
+128677757050683392 // jess // added from request
+533518661600673792 // glorp // groomed 15 yo
+1258378153777430561 // cunt / xrxexs // added bc he faked his suicide, has nsfw art channel in his own discord despite being like 15
+697637504744161302 // dtruut123 // allows porn in server full of minors
+1301708961459867733 // 1x.x7 // posts furry porn art around minors
+701632642822701057 // ftnick // monsterlike gooner
+969519949263544354 // awake115 // posts borderline nsfw in a server with minors
+789894523240841277 // re7orm // posted gore in BRING PF DOWN
+892000328751386674 // celest alt
+1053352529037119498 // crxxper // creep
+1256648260500852870 // alt account of presumably a stylis employee
+1280559356844310631 // stylisd3automaton
+1040443879771615322 // cid9 / stylis.cid9 // previously shays sister?
+// i would put wabbit here but he hops alts. so, futile
+456226577798135808 // idk who this is, possibly willow
+215538683384692736 // mar // pedophile
+703566926508589136 // shelby
+713409494914433084 // chairm4n // re7orm alt
+289169665341652993 // tyler_s (NOT tyler_fb / fleasion tyler) // sent fetish content to minors
